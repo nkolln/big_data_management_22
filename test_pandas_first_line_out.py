231cf62a4c6b1c2cef485d176e1ce9cc92fa816f
nkolln <n.m.p.kolln@student.tue.nl>
2022-02-28 15:08:01 +0100

    small changes

--- test_pandas_first_line_out.py
+++ test_pandas_first_line_out.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-txt_file = pd.read_csv('Data/MS1.txt', nrows=1000)
+txt_file = pd.read_csv('C:/Users/nickk/Downloads/weather-data/out_2018.txt', nrows=1000)
 txt_file.to_csv('Data/head.csv')
--- test_spark.py
+++ test_spark.py
@@ -2,4 +2,9 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.getOrCreate()
 textFile = spark.read.text("Data/MS1.txt")
-print(textFile.filter(textFile.value.contains("NASDAQ")).count())
+filt = "S&P"
+text_filter = textFile.filter(textFile.value.contains(filt)|textFile.value.contains(filt.upper())|textFile.value.contains(filt.lower()))
+print(text_filter.count())
+#text_filter = text_filter.filter(text_filter.value.contains('car'))
+#print(text_filter.count())
+text_filter.toPandas().to_csv('Data/'+filt+'.csv')
